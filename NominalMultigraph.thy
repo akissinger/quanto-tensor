6833dcb45599ca07579b9aac96c36d6c04ddb5dc
Aleks Kissinger <aleks0@gmail.com>
2014-02-06 09:07:29 +0000

    proved renaming product theorem

--- NominalMultigraph.thy
+++ NominalMultigraph.thy
@@ -3,12 +3,11 @@ imports "NomUtil"
 begin
 
 class nmts =
-fixes prod :: "('a::pt) \<Rightarrow> 'a \<Rightarrow> 'a" (infixr "\<cdot>" 72)
+fixes prod :: "('a::fs) \<Rightarrow> 'a \<Rightarrow> 'a" (infixr "\<cdot>" 72)
   and bnd  :: "name \<Rightarrow> 'a \<Rightarrow> 'a" ("\<nu> _ . _" [70,70] 70)
   and id   :: "name \<Rightarrow> name \<Rightarrow> 'a" ("\<delta>[_,_]" [74,74] 80)
   and one  :: 'a ("\<one>")
-assumes supp_fin: "finite (supp (x::'a))"
-    and prod_eqvt: "p \<bullet> (x \<cdot> y) = (p \<bullet> x) \<cdot> (p \<bullet> y)"
+assumes prod_eqvt: "p \<bullet> (x \<cdot> y) = (p \<bullet> x) \<cdot> (p \<bullet> y)"
     and bnd_eqvt[eqvt]: "p \<bullet> (\<nu> a . x) = \<nu> p \<bullet> a . p \<bullet> x"
     and id_eqvt[eqvt]: "p \<bullet> \<delta>[a,b] = \<delta>[p \<bullet> a,p \<bullet> b]"
     and one_eqvt[eqvt]: "p \<bullet> \<one> = \<one>"
@@ -26,6 +25,9 @@ assumes supp_fin: "finite (supp (x::'a))"
 lemma id_swp2[intro]: "x \<cdot> \<delta>[a,b] = ((a \<leftrightarrow> b) \<bullet> x) \<cdot> \<delta>[a,b]"
 by (metis id_swp prod_comm)
 
+lemma bnd_prod1: "(atom a) \<sharp> x \<Longrightarrow> \<nu> a . x \<cdot> y = x \<cdot> (\<nu> a . y)"
+by (metis atom_name_def bnd_prod prod_comm)
+
 lemma id_symm[intro]: "\<delta>[a,b] = \<delta>[b,a]"
 proof -
 have "\<delta>[a,b] = \<delta>[a,b] \<cdot> \<delta>[a,b]" by simp
@@ -42,6 +44,9 @@ by (metis prod_comm prod_unit)
 lemma supp_one[simp]: "supp \<one> = {}"
 by(simp add:eqvt_def supp_fun_eqvt)
 
+lemma id_fr[simp]: "atom c \<sharp> (a,b) \<Longrightarrow> atom c \<sharp> \<delta>[a,b]"
+by (auto simp:fr fresh_fun_app[where f = "id a"])
+
 lemma bnd_fr1[intro]:
   assumes [fr]: "at \<sharp> x"
   shows "at \<sharp> \<nu> a . x"
@@ -56,6 +61,15 @@ case False
     apply(simp_all add:fr)
   done
 qed
+
+lemma prod_fresh[intro]:
+  assumes [fr]: "at \<sharp> x"
+      and [fr]: "at \<sharp> y"
+    shows "at \<sharp> x \<cdot> y"
+apply(subst fresh_fun_app[where f = "prod x"])
+apply(subst fresh_fun_app[where f = "prod"])
+apply(auto simp:prod_eqvt permute_fun_def eqvt_def fr)
+done
 
 lemma bnd_supp[simp]: "supp (\<nu> a . x) \<subseteq> supp x"
 by (auto, metis bnd_fr1 fresh_def)
@@ -90,8 +104,9 @@ theorem gc:
   shows "\<nu> a . x = dim \<cdot> x"
 proof -
 have "\<nu> a . x = \<nu> a . \<one> \<cdot> x" by (metis prod_comm prod_unit)
-also have "... = (\<nu> a . \<one>) \<cdot> x" by (metis assms bnd_prod) 
-finally show ?thesis by simp
+also have "... = (\<nu> a . \<one>) \<cdot> x" by (metis assms bnd_prod)
+also have "... = dim \<cdot> x" by simp
+finally show ?thesis .
 qed
 
 theorem rnm_swap:
@@ -105,6 +120,36 @@ also have "... = \<nu> b . \<delta>[a,b] \<cdot> (a \<leftrightarrow> b) \<bulle
 also have "... = (\<nu> b . \<delta>[a,b]) \<cdot> (a \<leftrightarrow> b) \<bullet> x" by (metis bnd_prod fr)
 also have "... = \<one> \<cdot> (a \<leftrightarrow> b) \<bullet> x" by (metis assms fresh_Pair fresh_symm id_elim fr)
 also have "... = (a \<leftrightarrow> b) \<bullet> x" by simp
+finally show ?thesis .
+qed
+
+theorem rnm_prod:
+assumes [fr]: "(atom b) \<sharp> a"
+shows "[b >> a](x \<cdot> y) = [b >> a]x \<cdot> [b >> a]y"
+proof -
+obtain c :: name where [fr]: "(atom c) \<sharp> (a, b, x, y)" by (rule obtain_fresh)
+have [fr]: "atom c \<sharp> \<delta>[a,b]" by (simp add:fr)
+have [fr]: "atom c \<sharp> \<nu> b . \<delta>[a, b] \<cdot> y"
+by (metis fr bnd_fr1 fresh_Pair prod_fresh)
+have [fr]:"atom b \<sharp> (c, (b \<leftrightarrow> c) \<bullet> y)"
+by (metis fr flip_at_simps(2) fresh_Pair fresh_at_base_permute_iff fresh_symm)
+have delta:"\<delta>[a,b] \<cdot> \<delta>[b,c] = \<delta>[a,b] \<cdot> \<delta>[a,c]"
+by(subst id_swp, auto simp add:fr newfresh)
+
+have "[b >> a](x \<cdot> y) = \<nu> b . \<delta>[a,b] \<cdot> x \<cdot> y" by (simp add:rnm_def)
+also have "... = \<nu> b . \<delta>[a,b] \<cdot> x \<cdot> [c >> b]((b \<leftrightarrow> c) \<bullet> y)" by (metis fr rnm_swap permute_flip_cancel)
+also have "... = \<nu> b . \<delta>[a,b] \<cdot> x \<cdot> (\<nu> c . \<delta>[b,c] \<cdot> (b \<leftrightarrow> c) \<bullet> y)" by (simp add:rnm_def)
+also have "... = \<nu> b . \<nu> c . \<delta>[a,b] \<cdot> x \<cdot> \<delta>[b,c] \<cdot> (b \<leftrightarrow> c) \<bullet> y" by (metis fr fresh_Pair bnd_prod prod_comm)
+also have "... = \<nu> b . \<nu> c . \<delta>[a,b] \<cdot> \<delta>[b,c] \<cdot> x \<cdot> (b \<leftrightarrow> c) \<bullet> y" by (metis prod_comm prod_assoc)
+also have "... = \<nu> b . \<nu> c . \<delta>[a,b] \<cdot> \<delta>[a,c] \<cdot> x \<cdot> (b \<leftrightarrow> c) \<bullet> y" by (metis delta prod_assoc)
+also have "... = \<nu> b . \<delta>[a,b] \<cdot> (\<nu> c . \<delta>[a,c] \<cdot> x \<cdot> (b \<leftrightarrow> c) \<bullet> y)" by (metis fr bnd_prod prod_comm)
+also have "... = \<nu> b . \<delta>[a,b] \<cdot> (\<nu> c . x \<cdot> \<delta>[a,c] \<cdot> (b \<leftrightarrow> c) \<bullet> y)" by (metis prod_assoc prod_comm)
+also have "... = \<nu> b . (\<delta>[a,b] \<cdot> x) \<cdot> (\<nu> c . \<delta>[a,c] \<cdot> (b \<leftrightarrow> c) \<bullet> y)" by (auto simp:fr bnd_prod1)
+also have "... = (\<nu> b . \<delta>[a,b] \<cdot> x) \<cdot> (\<nu> c . \<delta>[a,c] \<cdot> (b \<leftrightarrow> c) \<bullet> y)" by (metis fr bnd_prod bnd_fr1 fresh_Pair id_fr prod_fresh)
+also have "... = (\<nu> b . \<delta>[a,b] \<cdot> x) \<cdot> (b \<leftrightarrow> c) \<bullet> (\<nu> b . (b \<leftrightarrow> c) \<bullet> \<delta>[a,c] \<cdot> y)" by (metis bnd_eqvt permute_flip_cancel prod_eqvt flip_at_simps(2))
+also have "... = (\<nu> b . \<delta>[a,b] \<cdot> x) \<cdot> (b \<leftrightarrow> c) \<bullet> (\<nu> b . \<delta>[a, b] \<cdot> y)" by (auto simp:flip_fresh_fresh fr)
+also have "... = (\<nu> b . \<delta>[a,b] \<cdot> x) \<cdot> (\<nu> b . \<delta>[a, b] \<cdot> y)" by(auto simp:fr flip_fresh_fresh)
+also have "... = [b >> a]x \<cdot> [b >> a]y" by (simp add:rnm_def)
 finally show ?thesis .
 qed
 
